3c04084bb6ad0cbb1801db08af7edb6361efd807
chdaud78 <qkrri56@naver.com>
2021-07-27 13:46:02 +0900

    [ENH] Update aside

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -1,6 +1,12 @@
-$(function() {
-   $('.nav-close').click(function(e) {
-      e.preventDefault();
-      $('.aside-nav').css('display', 'none');
-   });
+$(function () {
+    $('.nav-close').click(function (e) {
+        e.preventDefault();
+        $('.aside-nav').css('display', 'none');
+        $('.aside-nav-menu').css('opacity', '1');
+    });
+    $('.aside-nav-menu').click(function (e) {
+        e.preventDefault();
+        $('.aside-nav').css('display', 'block');
+        $('.aside-nav-menu').css('opacity', '0');
+    });
 });
